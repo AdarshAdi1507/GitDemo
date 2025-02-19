094bcc80b7291b39a3714b5d6afd5a3440f1d791
AdarshAdi1507 <adarshadi979@gmail.com>
2025-02-19 16:15:57 +0530

    dev changes

--- src/main/java/com/learn/spring/GitTest.java
+++ src/main/java/com/learn/spring/GitTest.java
@@ -5,5 +5,9 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("a new class and a new method has been added for testing");
 	}
+	
+	public void gitdevsaving() {
+		System.out.println("we need to save this to dev so we are saving it into dev");
+	}
 
 }
